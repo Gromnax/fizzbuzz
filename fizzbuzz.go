43f77eede65eb6db49796a3919ab17a32163d9d2
Gromnax <hezon.morgan@orange.fr>
2024-02-23 18:19:24 +0100

    Cleaned code with only last versions

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -7,61 +7,6 @@ import ("fmt"
 )
 
 func Fizzbuzz(a int) string {
-	// Test 1 & 2
-	/*if a==1 {
-		return "1"
-	}
-	if a==3 {
-		return "3 => Fizz"
-	}
-	return ""*/
-	// +Test 3 & 4
-	/*fizzbuzz := ""
-	if a%3 == 0 && a%5 == 0 {
-		fizzbuzz = "FizzBuzz"
-	} else if a%3 == 0 {
-		fizzbuzz = "Fizz"
-	} else if a%5 == 0 {
-		fizzbuzz = "Buzz"
-	} else {
-		fizzbuzz = fmt.Sprintf("%d",a)
-	}
-	return fizzbuzz*/
-	//Refactor Test 4
-	/*fizzbuzz := ""
-	if a%3 == 0 {
-		fizzbuzz += "Fizz"
-	} 
-	if a%5 == 0 {
-		fizzbuzz += "Buzz"
-	}
-	if len(fizzbuzz) == 0 {
-		fizzbuzz = fmt.Sprintf("%d",a)
-	}
-	return fizzbuzz*/
-	// Tests 5 à 10 (tous les cas de figure sont testés)
-	/*fizzbuzz := ""
-	if a%3 == 0 {
-		fizzbuzz += "Fizz"
-	} else {
-		toString := fmt.Sprintf("%d",a)
-		if strings.Contains(toString, "3") {
-			fizzbuzz += "Fizz"
-		}
-	}
-	if a%5 == 0 {
-		fizzbuzz += "Buzz"
-	} else {
-		toString := fmt.Sprintf("%d",a)
-		if strings.Contains(toString, "5") {
-			fizzbuzz += "Buzz"
-		}
-	}
-	if len(fizzbuzz) == 0 {
-		fizzbuzz = fmt.Sprintf("%d",a)
-	}
-	return fizzbuzz*/
-	//On refactorise le Fizz et le Buzz chacune dans une fonction séparée pour plus de lisibilité
 	fizzbuzz := ""
 	fizzbuzz += Fizz(a)
 	fizzbuzz += Buzz(a)
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -6,8 +6,6 @@ import ("testing"
 )
 
 func TestFizzBuzz(t *testing.T) {
-	//Étape 1
-	/*
 	t.Run("1. Entering a number not fizz nor buzz", func(t *testing.T) {
 		got := Fizzbuzz(1)
 		want := "1"
@@ -27,47 +25,34 @@ func TestFizzBuzz(t *testing.T) {
 		got := Fizzbuzz(15)
 		want := "FizzBuzz"
 		assertCorrectMessage(t,got,want)
-	})*/
-	//Refactor des tests une fois l'étape 1 validée ; les tests sont dans l'ordre, le commentaire au dessus de chaque jeu.
-	cases := []struct {
-		Number int
-		FizzBuzzed  string
-	}{
-		//Teste si un nombre non FizzBuzz est renvoyé tel quel
-		{Number: 1, FizzBuzzed: "1"},
-		{Number: 2, FizzBuzzed: "2"},
-		//Teste si un nombre est Fizz
-		{Number: 3, FizzBuzzed: "Fizz"},
-		//Teste si un nombre est Buzz
-		{Number: 5, FizzBuzzed: "Buzz"},
-		//Teste si un nombre est FizzBuzz
-		{Number: 15, FizzBuzzed: "FizzBuzz"},
-		//Teste si 13 est bien Fizz (présence d'un 3, non divisible par 3)
-		{Number: 13, FizzBuzzed: "Fizz"},
-		//Teste si 52 est bien Buzz (présence d'un 5, non divisible par 5)
-		{Number: 52, FizzBuzzed: "Buzz"},
-		//Teste si 51 est bien FizzBuzz (présence d'un 5, divisible par 3, pas par 5)
-		{Number: 51, FizzBuzzed: "FizzBuzz"},
-		// Teste si 130 est bien FizzBuzz (présence d'un 3, divisible par 5, pas par 3)
-		{Number: 130, FizzBuzzed: "FizzBuzz"},
-		// Teste si 358 est bien FizzBuzz (présence d'un 3 et d'un 5, divisible ni par 3 ni par 5)
-		{Number: 358, FizzBuzzed: "FizzBuzz"},
-	}
-	for _, test := range cases {
-		t.Run(fmt.Sprintf("%d gets converted to %q", test.Number, test.FizzBuzzed), func(t *testing.T) {
-			got := Fizzbuzz(test.Number)
-			assertCorrectMessage(t,got,test.FizzBuzzed)
-		})
-	}
-	//Premier test de l'étape 2, ajouté à la série de tests.
-	/*t.Run("5. 13 is now a Fizz number", func(t *testing.T) {
+	})
+	t.Run("5. Entering a fizz number containing a 3, but not divisible by 3", func(t *testing.T) {
 		got := Fizzbuzz(13)
 		want := "Fizz"
 		assertCorrectMessage(t,got,want)
-	})*/
+	})
+	t.Run("6. Entering a buzz number containing a 5, but not divisible by 5", func(t *testing.T) {
+		got := Fizzbuzz(52)
+		want := "Buzz"
+		assertCorrectMessage(t,got,want)
+	})
+	t.Run("7. Entering a fizzbuzz number containing a 5, divisible by 3, but not divisible by 5", func(t *testing.T) {
+		got := Fizzbuzz(51)
+		want := "FizzBuzz"
+		assertCorrectMessage(t,got,want)
+	})
+	t.Run("8. Entering a fizzbuzz number containing a 3, divisible byb 5, but not divisible by 3", func(t *testing.T) {
+		got := Fizzbuzz(130)
+		want := "FizzBuzz"
+		assertCorrectMessage(t,got,want)
+	})
+	t.Run("9. Entering a fizzbuzz number containing a 3 and a 5 but divisible by neither", func(t *testing.T) {
+		got := Fizzbuzz(358)
+		want := "FizzBuzz"
+		assertCorrectMessage(t,got,want)
+	})
 }
 
-//Fonction pour tester l'étape 3
 func TestCompute(t *testing.T){
 	buffer := bytes.Buffer{}
 	Compute(&buffer)
